0a1febe276fd8e9a4ade7e5531157bd3ecae5390
Aybars Hasan Şalvarcı <aybarsalvarci44@gmail.com>
2025-03-04 23:06:14 +0300

    Add project files.

---
+++ netAngularJanuary1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*
+ * 
+ * 1 - C# programlama dili nedir, nerelerde kullanılır?
+ * 
+ *   Microsoft tarafından geliştirilen C tabanlı programlama dilidir. Nesne yönelimli ve tip güvenli bir dildir. 
+ *   Mobil uygulamalar, web siteleri, oyunlar, gömülü yazılımlar, masaüstü uygulamaları vb. alanlarda kullanılır.
+ *   
+ * 2 - Bir programın çalışması için temel bileşenler nelerdir?
+ * 
+ *      - Programı çalıştıracak bir işlemci.
+ *      - Programın üzerinde çalışağı ram.
+ *      - Programı depolayacak sabit disk.
+ *      - Programı makine diline çevirecek derleyici.
+ *      - Programın grafik işlem ihtiyacına göre bir grafik kartı.
+ *      
+ * 3 - Kullanıcıdan adını alıp ekrana yazdıran program.
+ * */
+
+Console.Write("İsim giriniz: ");
+
+string? Name = Console.ReadLine();
+
+Console.WriteLine($"İsminiz: {Name}");
